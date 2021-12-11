72486e82013a225be3d25cf6ac3fee1677d44318
Jair <jairaceves56@gmail.com>
2021-12-11 10:26:09 -0600

    Estilos a los Botones creados

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,14 +1,12 @@
 <template>
-  <h2>Counter</h2>
-  <p> {{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
+    <h2>Counter</h2>
+    <p> {{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
     
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+    <div>
+        <button>+1</button> 
+        <button>-1</button>
+    </div>
+
 </template>
 
 <script>
@@ -34,5 +32,21 @@
 </script>
 
 <style>
+
+    button{
+        background-color: #64BB87;
+        border-radius: 5px;
+        border: 1px white;
+        border-color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 10px 20px;
+        transition: 0.3s ease-in-out;
+    }
+    button:hover{
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
+
 
 </style>
