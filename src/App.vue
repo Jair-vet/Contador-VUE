fbbf97a25091b5fb4cedd6c92bc7b97a85ce54ac
Jair <jairaceves56@gmail.com>
2021-12-11 09:33:40 -0600

    Importando nuetro primer componente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+
+  <Counter />
+
+
 </template>
 
 <script>
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
+  components: {
+    Counter
+  }
 }
 </script>
 
